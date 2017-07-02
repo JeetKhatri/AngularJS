6e9c64bd80192f4e17f187eeab3686cd21089563
Jeet Khatri <jeet.g.khatri@gmail.com>
2017-07-02 17:08:45 +0530

    controller as syntax

--- AngularJS_Prectice/app/1stController.js
+++ AngularJS_Prectice/app/1stController.js
@@ -6,3 +6,10 @@ myApp.controller('1stController',function($scope){
   $scope.cost = 30;
   $scope.student = {firstname:'Jeet',lastname:'Khatri',rollno:201612024};
 });
+
+myApp.controller('2stController',function(){
+  this.message ="Welcome";
+  this.names=['jeet','harsh','arpan','sharvil','avnish'];
+  this.cost = 30;
+  this.student = {firstname:'Jeet',lastname:'Khatri',rollno:201612024};
+});
